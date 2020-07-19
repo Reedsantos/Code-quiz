46f5cd28d59fe91d292420ef71664cb9cf47affa
Reed Santos <66302441+Reedsantos@users.noreply.github.com>
2020-07-19 19:19:18 -0400

    Specified questions

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#questionsDiv");
+
 var score = 0;
 var questionIndex = 0;
-var secondsLeft = 60;
+var secondsLeft = 90;
 var timerStart = false;
 var penalty = 10;
 var ulCreate = document.createElement("ul");
 
 var questions = [
     {
-        ask: "LOREM IPSUM?",
-        options: ["a", "b", "c", "d"],
-        answer: "a"
+        ask: "What is the capital of California?",
+        options: ["Sacramento", "Los Angeles", "San Diego", "San Francisco"],
+        answer: "Sacramento"
     },
     {
-        ask: "test?",
-        options: ["1", "2", "3", "4"],
-        answer: "1"
+        ask: "What ancient civilization began between the Tigris and Euphrates rivers?",
+        options: ["Rome", "Greece", "Mesopotamia", "Egypt"],
+        answer: "Mesopotamia"
     },
+    {
+        ask: "How many Great Lakes are there?",
+        options: ["Three", "Five", "Seven", "Nine"],
+        answer: "Five"
+    },
+    {
+        ask: "What is the smallest Great Lake?",
+        options: ["Lake Huron", "Lake Michigan", "Lake Erie", "Lake Ontario"],
+        answer: "Lake Ontario"
+    },
+    {
+        ask: "The United Kingdoms is made up of how many countries?",
+        options: ["Two", "Three", "Four", "Five"],
+        answer: "Four"
+    },
+    {
+        ask: "Which state is the Grand Canyon located in?",
+        options: ["New Mexico", "Arizona", "Nevada", "Utah"],
+        answer: "Arizona"
+    },
+    {
+        ask: "What is the only vowel not used as the first letter in a U.S. State",
+        options: ["A", "E", "I", "U"],
+        answer: "E"
+    },
+
 ];
+
 timer.addEventListener("click", function () {
     if (timerStart === false) {
         timerStart = setInterval(function () {
@@ -35,6 +63,7 @@ timer.addEventListener("click", function () {
     }
     render(questionIndex);
 });
+
 function render(questionIndex) {
 
     questionsDiv.innerHTML = "";
@@ -53,6 +82,7 @@ function render(questionIndex) {
         listItem.addEventListener("click", (compare));
     })
 }
+
 function compare(event) {
     var element = event.target;
 
@@ -78,7 +108,7 @@ function compare(event) {
     questionsDiv.appendChild(createDiv);
 }
 
-function scorePage(){
+function scorePage() {
     questionsDiv.innerHTML = "";
     currentTime.innerHTML = "";
 
@@ -106,7 +136,6 @@ function scorePage(){
     newLabel.textContent = "Enter your initials: ";
     questionsDiv.appendChild(newLabel);
 
-
     var newInput = document.createElement("input");
     newInput.setAttribute("type", "text");
     newInput.setAttribute("id", "initials");
@@ -118,33 +147,36 @@ function scorePage(){
     newSubmit.setAttribute("id", "Submit");
     newSubmit.textContent = "Submit";
     questionsDiv.appendChild(newSubmit);
-}
-    
+
     newSubmit.addEventListener("click", function () {
-    var initials = newInput.value;
+        var initials = newInput.value;
 
-    if (initials === null) {
+        if (initials === null) {
 
-        console.log("No value entered!");
-
-    } else {
-        var finalScore = {
-            initials: initials,
-            score: timeRemaining
-        }
-        console.log(finalScore);
-        var allScores = localStorage.getItem("allScores");
-        
-        if (allScores === null) {
-            allScores = [];
+            console.log("No value entered!");
 
         } else {
-            allScores = JSON.parse(allScores);
-        }
-        allScores.push(finalScore);
-        var newScore = JSON.stringify(allScores);
-        localStorage.setItem("allScores", newScore);
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+
+            console.log(finalScore);
+
+            var allScores = localStorage.getItem("allScores");
+            
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
         }
 
     });
+
+}
+
 
